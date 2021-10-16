58d683117e5fceede67a6614b6da46c35e845b5b
atena ganji <atena.ganji.ag@gmail.com>
2021-10-16 08:28:49 +0330

    final

--- EX3/part1/index.cpp
+++ EX3/part1/index.cpp
@@ -2,18 +2,20 @@
 #include <avr/delay.h>
 #define F_CPU16000000UL
 
-char 7segment[10]={
+char segment[10]={
     0x3f,0x06,0x5b,0x47,0x66,0x4d,0x7d,0x07
-}
+};
 
-int main(void){
+int main() 
+{
     PORTC=0xff;
- int i=0;
-
+    int i=0;
+    _delay_ms(1000);
     while (1)
     {
-       PORTC=7segment[i];
+       i+=1;
+       PORTC=segment[i];
        if(i>10) 
-       i=0
+       i=0;
     }
 }
